ca884b70627aaba7b9c06fdaa824b9a95ac0f1d5
jolestar <jolestar@gmail.com>
2019-04-15 23:39:06 +0800

    refactor move out sdk and game room logic.

--- server.py
+++ server.py
@@ -1,11 +1,31 @@
+#! /usr/bin/env python
+
 import SimpleHTTPServer
 import SocketServer
+import posixpath
+import argparse
+import urllib
+import os
 
-PORT = 5000
+PORT = 1234
 
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    pass
 
+    def translate_path(self, path):
+            path = posixpath.normpath(urllib.unquote(path))
+            words = path.split('/')
+            words = filter(None, words)
+            path = self.base_path
+            for word in words:
+                drive, word = os.path.splitdrive(word)
+                head, word = os.path.split(word)
+                if word in (os.curdir, os.pardir):
+                    continue
+                path = os.path.join(path, word)
+            return path
+
+
+Handler.base_path = "docs"
 Handler.extensions_map['.wasm'] = 'application/wasm'
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,7 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HelloComponent from "./components/Hello";
-import GamelobbyComponent from "./components/Gamelobby";
-import GameroomComponent from "./components/Gameroom";
-import MsgBus from "./components/Msgbus";
+import * as vm from "./vm";
 
-Vue.use(VueRouter)
+vm.init(1, function (state) {
+    console.log("state update", state)
+}, true);
 
-const Home = Vue.component("home", {template: '<div>home</div>'});
-
-
-const routes = [
-    {name: "home", path: '/', component: Home},
-    {name: "lobby", path: '/lobby', component: GamelobbyComponent},
-    {name: "room", path: '/room/:roomId', component: GameroomComponent, props: true},
-    {name: "hello", path: '/hello/:name/:initialEnthusiasm', component: HelloComponent, props: true}
-];
-
-const router = new VueRouter({
-    routes
-});
-
-
-const app = new Vue({
-    template: `
-        <div>
-        <h1>Hello App!</h1>
-            <p>
-            <router-link to="/">Home</router-link>
-            <router-link to="/hello/world/1">Hello World</router-link>
-            <router-link to="/lobby">Game Lobby</router-link>
-            </p>
-        <router-view></router-view>
-        </div>
-    `,
-    router,
-    created() {
-        console.log("app create", this.$refs);
-        MsgBus.init()
-    },
-    components: {}
-}).$mount('#app')
+vm.startGame();
